5b6d7aec4cdd20488999d51ec7213c1112e1f793
sulaiman <sulaiman@pop-os.localdomain>
2023-09-29 20:49:22 +0530

    continue upgrade

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
-//
+//continue
 
 for(let i=0;i<10;i++){
     
-    if(i==7 && i==9){
+    if(i==7 || i==8){
         continue;
         
     }
     console.log(i)
 }
+
+console.log("----------------------")
+
+//break
+
+
+for(let pop=1;pop<=15;pop++){
+    console.log(pop)
+    if(pop==8){
+        break;
+    }
+    
+}
+
 
 
